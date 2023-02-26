e274cbbc0229948e23640587b8eab0bb94fb63c8
Tony Wiredu <tonywiredu5@gmail.com>
2023-02-25 22:10:50 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ * Description: 'peint lowercase alphabets'
+ * Return: Always 0
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
