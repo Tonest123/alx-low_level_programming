507fc3a19e11bf3f367e09c21a634ea3c58c197e
root <tonywiredu5@gmail.com>
2023-02-27 23:52:46 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,29 +1,17 @@
 #include "main.h"
-/**
- * puts2 - function should print only one character of two
- * starting with the first one
- * @str: input
- * Return: print
-*/
-void put2(char *str)
-{
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
 
-	while (*y != '\0')
+/**
+ * puts2 - Prints every other character of a string, starting with the first
+ * @str: pointer to first character of string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *str; str++)
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0; o <= t; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
+		if (i++ % 2 == 0)
+			_putchar(*str);
 	}
 	_putchar('\n');
 }
