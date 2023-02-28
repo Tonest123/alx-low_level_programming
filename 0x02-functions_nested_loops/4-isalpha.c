eaee64a3d886785baaf99831e557aceaf0b7d213
root <tonywiredu5@gmail.com>
2023-02-28 14:36:33 -0800

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints +, 0, or - based on positive or negative
+ * @n: number o=to check
+ *
+ * Return: 1 for positive, 0 for zero, -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+
+	_putchar('0');
+	return (0);
+
+}
+
