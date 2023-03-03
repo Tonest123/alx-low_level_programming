91712ef1e8274a254eae1c638c6b9935b342acd8
root <tonywiredu5@gmail.com>
2023-03-02 17:28:38 -0800

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,10 @@
 #include "main.h"
+
 /**
  * _memset - fills memory with a constant byte
- *
  * @s: pointer to memory area to fill
  * @n: the number of bytes to fill
  * @b: constant byte
- *
  * Return: a pointer to the memory area @s
  */
 
@@ -17,6 +16,5 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
-
 	return (s);
 }
