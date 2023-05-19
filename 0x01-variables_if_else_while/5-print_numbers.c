53439d4b07e700c19eadb47a5a84ad3d5bf9cc4e
Tony Wiredu <tonywiredu5@gmail.com>
2023-05-19 02:13:10 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: 'Print all single numbers of base 10'
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
