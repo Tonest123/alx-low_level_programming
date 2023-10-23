3cecd659579702bd977bfa6d3438c3b0268c7538
Tonest123 <tonywiredu5@gmail.com>
2023-10-22 17:26:28 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,14 +3,14 @@
 
 /**
 * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
-*   is executed before main()
+* is executed before main()
 */
 void myStartupFun(void) __attribute__((constructor));
 
 
 /**
-* myStartupFun - implementation of myStartupFun
-*/
+  *myStartupFun - implementation of myStartupFun
+  */
 void myStartupFun(void)
 {
    printf("You're beat! and yet, you must allow,\n");
